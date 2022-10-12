aa0808e8f6f8738428f7804ec75928bf53eb14a4
LwFrahm <luke.2017@hotmail.com>
2022-10-12 13:13:42 -0500

    Luke Finished

--- social-media-angular/social-media-angular/src/app/models/Post.ts
+++ social-media-angular/social-media-angular/src/app/models/Post.ts
@@ -4,14 +4,16 @@ export default class Post {
     id: number
     text: string
     imageUrl: string
-    author: User
+    author!: User
     comments: Post[]
+    likes:User[]
 
-    constructor (id: number, text: string, imageUrl: string, author: User, comments: Post[]) {
+    constructor (id: number, text: string, imageUrl: string, author: User, comments: Post[], likes:User[]) {
         this.id = id
         this.text = text
         this.imageUrl = imageUrl
         this.author = author
         this.comments = comments
+        this.likes = likes
     }
 }
